942b9ba41a16466583b91b1367cd65945bcf261f
Sebastian Mercado M <sebastianandresmm2005@gmail.com>
2024-09-10 21:50:49 -0500

    main otra vez

--- src/laboratorio_1_ed/Laboratorio_1_ED.java
+++ src/laboratorio_1_ed/Laboratorio_1_ED.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package laboratorio_1_ed;
 
 import Frames.Inicio;
 
-/**
- *
- * @author ADALBERTO
- */
 public class Laboratorio_1_ED {
 
     /**
@@ -19,5 +11,4 @@ public class Laboratorio_1_ED {
        Inicio I = new Inicio();
        I.setVisible(true);
     }
-    
 }
